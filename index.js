e349f8a9944aebeeb6a02eb031d531d5a7bb5931
Victoria <victoriathorsenn@gmail.com>
2021-06-02 19:42:34 -0400

    commit tree 5dd3bc9a630b28d56ea74ebb0a2d0bc1975785ca

---
+++ index.js
@@ -0,0 +1,29 @@
+let routine = [wakeDog, leashDog, walkToPark, throwFrisbee, walkHome, unleashDog]
+
+function wakeDog(dogName, dogBreed) {
+    return `Wake ${dogName} the ${dogBreed}`
+}
+
+function leashDog(dogName, dogBreed) {
+    return `Leash ${dogName} the ${dogBreed}`
+}
+
+function walkToPark(dogName, dogBreed) {
+    return `Walk to the park with ${dogName} the ${dogBreed}`
+}
+
+function throwFrisbee(dogName, dogBreed) {
+    return `Throw the frisbee for ${dogName} the ${dogBreed}`
+}
+
+function walkHome(dogName, dogBreed) {
+    return `Walk home with ${dogName} the ${dogBreed}`
+}
+
+function unleashDog(dogName, dogBreed) {
+    return `Unleash ${dogName} the ${dogBreed}`
+}
+
+function exerciseDog(dogName, dogBreed) {
+    return routine.map(exercise => exercise(dogName, dogBreed))
+}
